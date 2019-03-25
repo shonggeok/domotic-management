1ec28434532909a012202975e87d4711897a110e
sineverba <2634842+sineverba@users.noreply.github.com>
2019-03-25 12:39:21 +0100

    Feat: add get public IP
    Add command to get update public IP every 5 minutes.

---
+++ app/Interfaces/PublicIPInterface.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sineverba
+ * Date: 25/03/2019
+ * Time: 08:13
+ */
+
+namespace App\Interfaces;
+
+interface PublicIPInterface extends BaseInterface
+{
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->register(RepositoryServiceProvider::class);
     }
 
     /**
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Solve #1071 - Specified key was too long; max key length is 767 bytes
+        Schema::defaultStringLength(191);
     }
 }
